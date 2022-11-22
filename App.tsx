4c659df82e8e070f9cffd9a7228d2b28d1203b47
jonnyband <jmsupernet1@gmail.com>
2022-11-22 15:55:48 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,18 +5,20 @@ import { Home } from './src/screens/Home/Home';
 import { Login} from './src/screens/Login/Login';
 import { Registration } from './src/screens/Registration/Registration';
 import { Members } from './src/screens/Members/Members';
+import { CartContent } from './src/context';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-   
+   <CartContent>
   <NavigationContainer>
     <Drawer.Navigator>
     <Drawer.Screen name="Home" component={Home}/>
     <Drawer.Screen name="Integrantes" component={Members}/>
     </Drawer.Navigator>
   </NavigationContainer>
+  </CartContent>
 
 );
   
