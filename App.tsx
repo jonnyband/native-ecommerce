08f7791b76316eba785ed6d1c0aa50be6f8a0817
Paulo Mayworm <111092088+PauloMayworm@users.noreply.github.com>
2022-11-23 19:48:35 -0300

    criando rotas

--- App.tsx
+++ App.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import { Home } from './src/screens/Home/Home';
-import { Login} from './src/screens/Login/Login';
-import { Registration } from './src/screens/Registration/Registration';
-import { Members } from './src/screens/Members/Members';
-import { CartContent } from './src/context';
+import Routes from './src/routes/routes';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { Home } from './src/screens/Home/Home';
+// import { Login} from './src/screens/Login/Login';
+// import { Registration } from './src/screens/Registration/Registration';
+// import { Members } from './src/screens/Members/Members';
+// import { CartContent } from './src/context';
 
-const Drawer = createDrawerNavigator();
+// const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-   <CartContent>
-  <NavigationContainer>
-    <Drawer.Navigator>
-    <Drawer.Screen name="Home" component={Home}/>
-    <Drawer.Screen name="Integrantes" component={Members}/>
-    </Drawer.Navigator>
-  </NavigationContainer>
-  </CartContent>
+    <Routes/>
+  //  <CartContent>
+  // <NavigationContainer>
+  //   <Drawer.Navigator>
+  //   <Drawer.Screen name="Home" component={Home}/>
+  //   <Drawer.Screen name="Integrantes" component={Members}/>
+  //   </Drawer.Navigator>
+  // </NavigationContainer>
+  // </CartContent>
 
 );
   
